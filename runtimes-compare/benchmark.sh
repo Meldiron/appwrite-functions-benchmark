bfabb91a37205cf9c2fe30e9b06b5ad5e43c6071
Matej Baco <matejbacocom@gmail.com>
2022-02-22 11:01:09 +0100

    Update

---
+++ runtimes-compare/benchmark.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+k6 run --vus 10 --duration 10s dumpsterTheServer.js
---
+++ runtimes-compare/config.js
@@ -0,0 +1,6 @@
+export const stressConfig = {
+  URL: "[YOUR_ENDPOINT]",
+  APISecret: "[YOUR_SECRET_KEY]",
+  ProjectID: "[YOUR_PROJECT_ID]",
+  functionID: "[YOUR_FUNCTION_ID]",
+};
